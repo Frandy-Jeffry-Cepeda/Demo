a36c0527dc66d4642e9012bb882c1a28f0074beb
Frandy Jeffry Cepeda <107950875+Frandy-Jeffry-Cepeda@users.noreply.github.com>
2024-11-19 16:48:30 -0600

    Update

--- Demo/src/main.tsx
+++ Demo/src/main.tsx
@@ -1,10 +1,16 @@
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Router from './router'
 import './index.css'
-import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router/>
+    </Provider>
   </StrictMode>,
 )
