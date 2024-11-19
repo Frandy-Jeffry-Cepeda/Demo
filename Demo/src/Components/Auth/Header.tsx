2cc06cc92ab4f549c13e9ca345ceb80c8d3f7c32
Frandy Jeffry Cepeda <107950875+Frandy-Jeffry-Cepeda@users.noreply.github.com>
2024-11-19 16:48:19 -0600

    Create Header.tsx

---
+++ Demo/src/Components/Auth/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <div className='bg-white shadow-2xl py-5 px-10'>
+        <h1 className="font-semibold text-2xl"><span className='font-bold mx-2'>LeanIX</span>Partner Academy</h1>
+    </div>
+  )
+}
