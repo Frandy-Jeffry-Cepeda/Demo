6980ba777dafe67eaadacb7c290df85a51a5587f
Frandy Jeffry Cepeda <107950875+Frandy-Jeffry-Cepeda@users.noreply.github.com>
2024-11-21 17:32:47 -0600

    Update

--- Demo/src/Components/Dashboard/Section2.tsx
+++ Demo/src/Components/Dashboard/Section2.tsx
@@ -1,5 +1,9 @@
 
-
+export const videos = [
+    { src: '../../src/Assets/Videos.jpg', title: 'About Bootstrap Admin', description: "we've been a focused on making a the sky" },
+    { src: '../../src/Assets/Videos2.jpg', title: 'A yellow sofa', description: "we've been a focused on making a the sky" },
+    { src: '../../src/Assets/videos3.avif', title: 'Our Camara Mega Set', description: "we've been a focused on making a the sky" },
+  ];
 
 export default function Section2() {
 
@@ -11,6 +15,25 @@ export default function Section2() {
             <span className="text-blue-500">View All Videos</span>
         </div>
     </div>
+
+    <div className="flex gap-10 justify-center">
+        {videos.map((video, index) => (
+            <div 
+            key={index}
+            className="flex flex-col w-full sm:w-1/2 lg:w-1/3 h-auto rounded-lg bg-gray-100 overflow-hidden shadow-md">
+            <img 
+                src={video.src} 
+                alt={video.title} 
+                className="w-full h-48 object-cover rounded-t-lg"
+            />
+            <div className="p-4">
+                <span className="font-bold text-lg">{video.title}</span>
+                <p className="text-gray-500">{video.description}</p>
+            </div>
+            </div>
+        ))}
+    </div>
+
     </>
   )
 }
