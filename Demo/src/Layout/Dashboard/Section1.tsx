54beebc46d24e0af629d9334a43106378dc1b81b
Frandy Jeffry Cepeda <107950875+Frandy-Jeffry-Cepeda@users.noreply.github.com>
2024-11-21 13:03:55 -0600

    Update

---
+++ Demo/src/Layout/Dashboard/Section1.tsx
@@ -0,0 +1,58 @@
+
+export default function Section1() {
+  return (
+    <div>
+
+      <h2 className="font-bold">
+        Admin Panel - How To Get Started Tutorial. Create a customizable SaaS Based applications and soltions
+        <span className=" text-sm text-gray-500"> 5 mins read</span>
+      </h2>
+
+      <div className="py-4">
+        <img 
+          src="../../src/Assets/meeting.webp" 
+          alt="Meeting" 
+          className="rounded-lg"
+        />
+      </div>
+
+      <p className="text-gray-500">
+        First, a disclaimer - the entire process of writing a blog post often takes
+        more than a couple of hours, even if yo can type eighty words per minute and you
+        writing skills are sharp. From the seed of the idea to finally hiting "Publish",
+        you might spend severeal days or maybe even a week "writing" a blog post, bug it's
+        important to spend those vital hours planning your post and even thinking abot 
+        <span className="text-blue-500"> Your Post </span>(yes, thinking counts as working if you're a blogger) 
+        before you actually write it.
+      </p>
+
+      <br/>
+
+      <p className="text-gray-500">
+        There's an old maxim that states, "<span className="font-semibold">No fun for the writer, no fun for the reader.
+        </span>"No matter what industry you're working in, as a blogger, you should live and die by this statement.
+      </p>
+
+      <br/>
+
+      <p className="text-gray-500">
+        Before you do any of the following steps, be sure to pick a topic tat actally interets you. Nothing - and 
+        <span className="text-blue-500"> I mean NOTHING </span> - will kill a blog post more effectively than a 
+        lack of enthusiasm from the writer. You can tell when a writer is bored by their sbject, and it's so 
+        cringe-worthy it's a little embarrasing.
+      </p>
+
+      <br />
+
+      <p className="text-gray-500">
+        I can hear your objections already. "But Dan, I have to blog for a cardboard box manufactring company." I feel
+        your pain, i really do. During the course of my career, I've written content for dozens of clients in some l
+        ess-than-thrilling industries (such as financial regulatory compliance and corporate hosing), but the hellmark 
+        of a professional blogger is the ability to write well about any topic, no matter how dry it may be. Bloggin 
+        is a lot easier, however, if you can muster at leat a little enthusiasm for the topic at hand.
+      </p>
+
+
+    </div>
+  )
+}
--- Demo/src/views/Dashboard.tsx
+++ Demo/src/views/Dashboard.tsx
@@ -1,8 +1,37 @@
+import Section1 from "@/Layout/Dashboard/Section1"
+import { BriefcaseBusiness, Grid2X2, MessageSquareText } from "lucide-react"
 
 export default function Dashboard() {
   return (
-    <div>
-        <h2>Test</h2>
+
+    <div className=" bg-white m-5">
+
+        <div className="flex gap-1 px-2 pt-10 pb-5 text-[12px] sm:gap-5 sm:px-10 sm:text-sm">
+          
+          <div className="flex items-center gap-1">
+            <Grid2X2 className="w-4 h-4 sm:w-6 sm:h-6"/>
+           <span> 06 April 2021 </span> 
+          </div>
+
+          <div className="flex items-center gap-1">
+            <BriefcaseBusiness className="w-4 h-4 sm:w-6 sm:h-6"/>
+           <span> Announcements </span> 
+          </div>
+
+          <div className="flex items-center gap-1">
+            <MessageSquareText className="w-4 h-4 sm:w-6 sm:h-6"/>
+           <span> 24 Comments </span> 
+          </div>
+
+        </div>
+
+        
+        <section className="flex gap-1 px-2 sm:px-10">
+            <article>
+              <Section1/>
+            </article>
+          </section>
+
     </div>
   )
 }
